368c8492690fce201b4779ae040fa5a97aefc201
Marie Flem H <marie_fh@hotmail.com>
2018-06-26 15:10:45 +0200

    fetch JSON och consol.loga resultat

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,23 @@
-async function searchByCriteria(searchCriteria) {
-  const baseURL = 'http://api.arbetsformedlingen.se/af/v0/';
-  const responseObject = await fetch(baseURL + searchCriteria);
-  const matches = await responseObject.json();
-  console.log(matches);
+/*async function searchByCriteria(searchCriteria) {
+  *const baseURL = 'http://api.arbetsformedlingen.se/af/v0/';
+  *const responseObject = await fetch(baseURL + searchCriteria);
+  *const matches = await responseObject.json();
+  *console.log(matches);
 }
 
 searchByCriteria('platsannonser/matchning?lanid=1&yrkesomradeid=3&antalrader=30');
+*/
+
+
+let platsannonser = [];
+
+async function getAnnonser() {
+  let url = 'http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?lanid=1';
+  fetch(url)
+  .then((response) => response.json())
+  .then((platsannonser) => {
+      console.log(platsannonser);
+  })
+}
+
+getAnnonser();
