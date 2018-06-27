906e168599adcf5fdba4bb779edd6dd4aea87678
Kajsa Stoll Kuroczik <25846144+kajsastollk@users.noreply.github.com>
2018-06-27 12:06:28 +0200

    Oklar commit, det finns evtl en mindre rad i JS

--- scripts/index.js
+++ scripts/index.js
@@ -8,28 +8,30 @@
 searchByCriteria('platsannonser/matchning?lanid=1&yrkesomradeid=3&antalrader=30');
 */
 
+const itURL = `http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?lanid=1&yrkesomradeid=3&antalrader=30`;
+
 
 let platsannonser = [];
 
 async function getAnnonser() {
   let url = 'http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?lanid=1&antalrader=50';
   fetch(url)
-  .then((response) => response.json())
-  .then((platsannonser) => {
+    .then((response) => response.json())
+    .then((platsannonser) => {
       createCards(platsannonser);
-  })
+    })
 }
 
 getAnnonser();
 
 function createCards(platsannonser) {
-  const container = document.querySelector('#allaAnnonser'); 
-  let html = ''; 
-  for(let annonser of platsannonser.matchningslista.matchningdata) {
+  const container = document.querySelector('#allaAnnonser');
+  let html = '';
+  for (let annonser of platsannonser.matchningslista.matchningdata) {
     html += `
     <div class="card" style="background-color: lightgrey">
     <p>${annonser.annonsrubrik}</p>
-    </div>`; 
+    </div>`;
   };
-  container.innerHTML = html; 
+  container.innerHTML = html;
 }
